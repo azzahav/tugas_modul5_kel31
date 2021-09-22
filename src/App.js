d126cca5af9b5d1e898a433e6da75e08b8f3efbd
Azzah Afifah Veronica <azzahafifahv@gmail.com>
2021-09-23 00:36:36 +0700

    Tugas Modul 5 :)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Shop from './components/product.js';
+import Listen from './components/listen.js';
+import 'antd/dist/antd.css';
+import { Layout, Typography } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Content, Footer } = Layout;
+const { Text } = Typography;
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+ <Layout style={{background:'#FFDAB9'}}>
+      <Header style={{background:'#CD853F'}}>
+        <Text style={{fontFamily:'Fantasy', color:'#FFFFFF', fontSize: '30px'}}>
+        ERIGO STORE
+        </Text>
+        <div style={{float: 'right'}}>
+          <Link to="/" style={{margin:'8px', fontFamily:'Roboto',fontSize: '20px', color:'#FFFFFF'}}>PRODUCT</Link>
+          <Link to="/listen" style={{margin:'8px', fontFamily:'Roboto',fontSize: '20px', color:'#FFFFFF'}}>LISTEN</Link>
+        </div>
+      </Header>
+      <Content style={{textAlign:'center', display: 'inline-block'}}>
+      <Switch>
+      <Route path="/" exact component={Shop} />
+      <Route path="/listen" exact component={Listen} />
+      </Switch>
+      </Content>
+      <Footer style={{background:'#CD853F', fontSize: '20px', textAlign:'center', width:'100%', fontFamily:'Roboto', color:'#FFFFFF'}}>
+          KELOMPOK 31 - Azzah - Caerel
+        </Footer>
+    </Layout>
+    </Router>
+    )
+  }
 }
-
 export default App;

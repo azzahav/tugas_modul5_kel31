e60be3fd96b01b6729a664839cbb8f442008ef65
Azzah Afifah Veronica <azzahafifahv@gmail.com>
2021-09-23 16:59:11 +0700

    Add

--- src/components/product.js
+++ src/components/product.js
@@ -17,6 +17,9 @@ export default class shop extends Component {
             add:"",
         };
     }
+    handleButton = (add) => {
+        alert("Halloo : " + add);
+            };
     componentDidMount() {
         axios({
             method: "get",
@@ -35,18 +38,21 @@ export default class shop extends Component {
                 console.log(error);
             });
     }
+    
     render() {
         return (
             <div>
                 {this.state.shop.map((results) => {
                     return (
                         <div key={results.id} style={{ textAlign: 'center', display: 'inline-block' }}>
-                            <Card hoverable onClick={results.add} style={{ textAlign: 'center', display: 'inline-block', width: 350, margin: '30px', boxShadow: '1px 2px 1px 0.1px', borderRadius: '20px' }}
+                            
+                            <Card hoverable onClick={() => this.handleButton(results.add)} style={{ textAlign: 'center', display: 'inline-block', width: 350, margin: '30px', boxShadow: '1px 2px 1px 0.1px', borderRadius: '20px' }}
                                 cover={<img alt="gambar1" maxWidth="30px" src={results.image_link} />}>
                                 <Meta title={results.name} description={results.price} />
                                 <div>
                                     <p>Brand : {results.brand}</p>
                                 </div>
+                                <button type="button" class="btn btn-primary" onClick={() => this.handleButton(results.add)}>Paket 1</button>
                             </Card>
                         </div>
                     );
